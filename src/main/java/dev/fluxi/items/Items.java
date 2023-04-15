9adad56179c6445c3f3a9a1ad102460f2830d62c
Felix <felix.waidler@web.de>
2023-04-15 23:52:02 +0200

    Made custom item category

--- src/main/java/dev/fluxi/items/Items.java
+++ src/main/java/dev/fluxi/items/Items.java
@@ -4,13 +4,20 @@ import dev.fluxi.NicotineMod;
 import dev.fluxi.items.custom.CigaretteItem;
 import dev.fluxi.items.custom.FilterTipPaperItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
 
 public class Items {
+    private static final ItemGroup ITEM_GROUP = FabricItemGroup.builder(new Identifier("nicotine", "main"))
+            .icon(() -> new ItemStack(Items.CIGARETTE)).build();
+
     public static Item CIGARETTE = null;
     public static Item ROLLING_PAPER = null;
     public static Item ACTIVATED_CHARCOAL_FILTER = null;
@@ -30,6 +37,7 @@ public class Items {
     }
 
     private static Item register(String identifier, Item item) {
+        ItemGroupEvents.modifyEntriesEvent(Items.ITEM_GROUP).register(content -> content.add(item));
         return Registry.register(Registries.ITEM, new Identifier(NicotineMod.MOD_ID, identifier), item);
     }
 }
